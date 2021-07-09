92f57b2cec3c675ab7672b74224b275bf33007ed
Mohammad Jafari <m.jafari9877@gmail.com>
2021-07-09 17:19:39 +0430

    should be authenticated in college list

--- college/views.py
+++ college/views.py
@@ -1,5 +1,5 @@
 from rest_framework import status
-from rest_framework.permissions import IsAdminUser, AllowAny
+from rest_framework.permissions import IsAdminUser, AllowAny, IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
 
@@ -71,7 +71,7 @@ class CollegeDetailView(APIView):
 
 class CollegesView(APIView):
     permission_classes = {
-        "get": [AllowAny],
+        "get": [IsAuthenticated],
         "post": [IsAdminUser]
     }
 
